43cb76460913476745d52769644006d8fe962a2c
JiHongKim15 <15computer@hanmail.net>
2022-02-01 21:34:05 +0900

    feature: 부트스트랩 적용

--- mbti-front/src/service/axios.service.ts
+++ mbti-front/src/service/axios.service.ts
@@ -3,7 +3,6 @@ import axios, { AxiosInstance } from 'axios';
 export default class AxiosServiceJiHong {
 
     static readonly Instance: AxiosInstance = axios.create({
-        //baseURL: 'http://localhost:8081',
         timeout: 10000,
     });
 }

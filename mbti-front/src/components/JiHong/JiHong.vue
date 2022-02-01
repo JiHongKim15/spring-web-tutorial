eb3503b38382b12638c2903f489090a005c41500
JiHongKim15 <15computer@hanmail.net>
2022-02-02 00:02:52 +0900

    feature: 카드 형식 연동

--- mbti-front/src/components/JiHong/JiHong.vue
+++ mbti-front/src/components/JiHong/JiHong.vue
@@ -1,36 +1,37 @@
 <template>
     <div id = "graph">
-        <thead>
-            <tr>
-                <th>제목</th>
-                <th>내용</th>
-            </tr>
-        </thead>
-        <!-- <tbody v-for="item in items" :key="item.title">
-            <td> <span v-html = "item.title"/> </td>
-            <td> <span v-html = "item.content"/> </td>
-        </tbody> -->
+        <b-card
+         v-for = "mainBoard in mainBoardList" :key = "mainBoard.title"
+         tag="article"
+         style="max-width: 20rem;"
+         class="mb-2"
+        >
+            <b-card-title>
+                {{mainBoard.title}}
+            </b-card-title>
+            <b-card-text>
+                {{mainBoard.content}}
+            </b-card-text>
+        </b-card>
     </div>
 </template>
 
 <script lang = "ts">
+import Vue from 'vue';
 import AxiosService from '../../service/axios.service';
-import AxiosResponse from '../../service/axios.service';
 
-class MainBaord {
-    public mainBoardNum?: number;
-    public content?: string;
-    public writer?: string;
-    public writeTime?: Date;
-}
-export default {
-    mainBoardList: [],
-    async mounted() {
-        const response: AxiosResponse = await AxiosService.Instance.get('/v1');
-        
-        console.log(response);
+export default Vue.extend({
+    data() {
+        return {
+            mainBoardList: [],
+        };
     },
-};
+    async mounted() {
+        const response = await AxiosService.Instance.get('/v1');
+        this.mainBoardList = response.data;
+    }
+    
+});
 </script>
 
 <style>

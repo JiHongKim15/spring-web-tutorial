a1f9851d13480bc246d09430adf12715911bd26a
imwisdom <kjihye0340@naver.com>
2022-02-02 21:13:42 +0900

    feature: header에 search 추가

--- mbti-front/src/components/Wisdom/WisdomHeader.vue
+++ mbti-front/src/components/Wisdom/WisdomHeader.vue
@@ -9,9 +9,16 @@
 
           <b-nav-item href="#">Sign Up</b-nav-item>
         </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
         <!-- Navbar dropdowns -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown text="MBTI" right>
+          <b-nav-item-dropdown text="MBTI">
             <b-dropdown-item href="#">ESTJ</b-dropdown-item>
             <b-dropdown-item href="#">ENFP</b-dropdown-item>
             <b-dropdown-item href="#">ISTP</b-dropdown-item>
